1f866f9ce297f64fc6aa86ac19b3122b633919ba
Tareck-Ali <tareckali@outlook.com>
2026-09-13 16:11:10 -0300

    change: fitting for natural split of data

--- train_baseline.py
+++ train_baseline.py
@@ -5,26 +5,16 @@ import pandas as pd
 
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.metrics import classification_report
-from sklearn.model_selection import train_test_split
 from sklearn.pipeline import Pipeline
 
 import settings
 
 
 def train():
-    df = pd.read_csv(settings.DATA)
+    df = pd.read_csv(settings.TRAIN_DATA)
 
     X = df["medical_abstract"].fillna("")
     y = df["condition_label"]
-
-    X_train, X_val, y_train, y_val = train_test_split(
-        X,
-        y,
-        test_size=0.2,
-        random_state=42,
-        stratify=y,
-    )
 
     model = Pipeline([
         (
@@ -47,17 +37,13 @@ def train():
         ),
     ])
 
-    model.fit(X_train, y_train)
-
-    predictions = model.predict(X_val)
-
-    #print(classification_report(y_val, predictions))
+    model.fit(X, y)
 
     Path(settings.MODELS).mkdir(parents=True, exist_ok=True)
 
     joblib.dump(model, settings.BASELINE_MODEL)
 
-    #print(f"Model saved to {settings.BASELINE_MODEL}")
+    print(f"Model saved to {settings.BASELINE_MODEL}")
 
 
 if __name__ == "__main__":
