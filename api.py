152e17ed922d6fd8205125efdbf0835ee23178fd
Tareck-Ali <tareckali@outlook.com>
2026-09-13 16:31:22 -0300

    add: api

---
+++ api.py
@@ -0,0 +1,32 @@
+import onnxruntime as ort
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+import settings
+
+
+app = FastAPI(title="Medical Abstract Classifier")
+
+
+session = ort.InferenceSession(
+    str(settings.ONNX_MODEL)
+)
+
+input_name = session.get_inputs()[0].name
+
+
+class PredictionRequest(BaseModel):
+    abstract: str
+
+
+@app.post("/predict")
+def predict(request: PredictionRequest):
+    result = session.run(
+        None,
+        {input_name: [request.abstract]},
+    )
+
+    return {
+        "prediction": result[0][0]
+    }
